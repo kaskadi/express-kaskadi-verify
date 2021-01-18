54b84067a2d49f51371d5fc1a03313355a346986
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 11:23:02 +0100

    Added JSDoc like comments for documentation

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 /**
- * You can use JSDoc comments in your `.js` files to generate automatically documentation for your package via the `generate-docs` workflow.
+ * Verification function.
  *
- * @module help
+ * Provided a set of options, this will return an Express middleware usable inside an Express app. When using this middleware for an endpoint, the decoded JWT will be attached to the `request` object from Express so that it can be used in your code.
+ *
+ * @function verify
+ *
+ * @param {Object} options - Options sent for request verification. This can contain the usual JWT claims (`audience`, `issuer`, `subject`) as well as their expected values.
+ * @return {function} Express middleware
+ *
+ */
+
+/**
+ * Configure a new verifier instance.
+ *
+ * @module verifier
+ *
+ * @param {string} url - URL to the verification server used for verifying incoming requests.
+ * @return {verify} Verification function
+ *
+ * @example
+ *
+ * const verifier = require('express-kaskadi-verify')('http://localhost:3000') // instanciate a new verifier
+ * const express = require('express')
+ * const app = express()
+ * const port = 4000
+ *
+ * app.get('/issuer-example', verify({ issuer: ['my-issuer'] }), (req, res) => {
+  res.json(req.user)
+}) // verify against the JWT issuer
+ *
+ * app.get('/admin', verify({audience: ['admin'] }), (req, res) => {
+  res.json(req.user)
+}) // verify against the JWT audience
+ *
+ * app.get('/some/subject', verify({subject: ['some'] }), (req, res) => {
+  res.json(req.user)
+}) // verify against the JWT subject
+ *
+ * app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
  *
  */
 
