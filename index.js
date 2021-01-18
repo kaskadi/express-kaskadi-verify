394a48f2792f6cda8381f65954d55099276f6225
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-18 11:01:46 +0100

    Implemented base logic

--- index.js
+++ index.js
@@ -5,4 +5,35 @@
  *
  */
 
-console.log('Hello World!')
+const fetch = require('node-fetch')
+
+function verifier (url) {
+  return function verify (options) {
+    const opt = JSON.stringify(options)
+    return async (req, res, next) => {
+      var result = await fetch(url, {
+        headers: {
+          'x-kaskadi-route-options': opt,
+          authorization: ' Bearer ' + getToken(req)
+        }
+      })
+      if (result.status === 200) {
+        req.user = (await result.json()).user
+        return next()
+      } else {
+        res.status(401).send(await result.json())
+      }
+    }
+  }
+}
+
+function getToken (req) {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    return req.headers.authorization.split(' ')[1]
+  } else if (req.query && req.query.token) {
+    return req.query.token
+  }
+  return null
+}
+
+module.exports = verifier
