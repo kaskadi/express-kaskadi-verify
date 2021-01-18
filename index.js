f2fcb8f0b3f8f883360392b106b8e290e96c80ca
Alexis Lemaire <50198634+alexlemaire@users.noreply.github.com>
2021-01-18 10:56:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * You can use JSDoc comments in your `.js` files to generate automatically documentation for your package via the `generate-docs` workflow.
+ *
+ * @module help
+ *
+ */
+
+console.log('Hello World!')
